705f4584db69cef140508ac07b34b616e700cafc
Belagum <140494656+Belagum@users.noreply.github.com>
2024-06-25 20:06:59 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,11 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+// Проверяем, существует ли директория
+if (!is_dir($target_dir)) {
+    mkdir($target_dir, 0777, true);
+}
 
