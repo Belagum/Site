73d628fac0526214c4d7dd3dcf3ba8d3ef42575f
Belagum <140494656+Belagum@users.noreply.github.com>
2024-06-26 06:54:02 +0300

    Update keyauth.js

--- keyauth.js
+++ keyauth.js
@@ -1,8 +1,8 @@
 const KeyAuthApp = {
-    "12", // Application Name
-    "n8shnhqmPc", // Owner ID
-    "975f8d25e4084b42adae80cdb330bba9ea6d0340c40e60311577447ed352f5af", // Application Secret
-    "1.0", // Application Version
+    name: "12", // Application Name
+    ownerid: "n8shnhqmPc", // Owner ID
+    secret: "975f8d25e4084b42adae80cdb330bba9ea6d0340c40e60311577447ed352f5af", // Application Secret
+    version: "1.0" // Application Version /*
 };
 
 document.getElementById('register-form').addEventListener('submit', function(event) {
