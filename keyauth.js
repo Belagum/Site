c506658eca72081dac8f081282d4be62c3f7bb4c
Belagum <140494656+Belagum@users.noreply.github.com>
2024-06-26 06:41:59 +0300

    Update keyauth.js

--- keyauth.js
+++ keyauth.js
@@ -1,16 +1,22 @@
 document.getElementById('register-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    console.log('Форма отправлена');
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const licenseKey = document.getElementById('license-key').value;
 
-    const KeyAuthApp = new KeyAuth(
+    console.log('username:', username);
+    console.log('password:', password);
+    console.log('licenseKey:', licenseKey);
+
+    const keyAuthApp = {
     "12", // Application Name
     "n8shnhqmPc", // Owner ID
     "975f8d25e4084b42adae80cdb330bba9ea6d0340c40e60311577447ed352f5af", // Application Secret
     "1.0", // Application Version
-    );
+    };
 
     fetch('https://keyauth.win/api/1.0/register/', {
         method: 'POST',
@@ -30,6 +36,7 @@ document.getElementById('register-form').addEventListener('submit', function(eve
     })
     .then(response => response.json())
     .then(data => {
+        console.log('Ответ сервера:', data);
         if (data.success) {
             localStorage.setItem('username', username);
             localStorage.setItem('password', password);
